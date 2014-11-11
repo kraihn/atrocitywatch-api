6c8e372f624326eec2855ae72685e7d19350cc14
Jared Dickson <code@jareddickson.com>
2014-11-11 13:37:44 -0800

    Init status

--- routes/status.js
+++ routes/status.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send({status: 'safe'});
+    if (req.query && (req.query.lat && req.query.lng)) {
+        // TODO: get data
+        res.send({success: true, safe: true})
+    }
+    else {
+        res.send(400, {success: false});
+    }
 });
 
 module.exports = router;
