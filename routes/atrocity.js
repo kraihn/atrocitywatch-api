ff008d942cbabd0f9d769ebca0399ee5ff025752
Jared Dickson <code@jareddickson.com>
2014-11-11 13:36:38 -0800

    Init new atrocity

--- routes/atrocity.js
+++ routes/atrocity.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function (req, res) {
-    res.send({success: true});
+    if (req.body && (req.body.lat && req.body.lng && req.body.type && req.body.description)) {
+        var atrocity = {
+            date: new Date(),
+            latitude: req.body.lat,
+            longitude: req.body.lng,
+            type: req.body.type.split(','),
+            description: req.body.description,
+            severity: req.body.severity,
+            radius: 25
+        };
+        // TODO: record data to the database
+        res.send({success: true})
+    }
+    else {
+        res.send(400, {success: false});
+    }
 });
 
 module.exports = router;
