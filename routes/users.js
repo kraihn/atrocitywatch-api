1c9f8743576c5b45b70569b7dae2fbec6284287f
Jared Dickson <code@jareddickson.com>
2014-11-11 13:36:54 -0800

    Init get users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Get users in hotspot
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    if (req.query && (req.query.lat && req.query.lng)) {
+        // TODO: get users
+        res.send({success: true})
+    }
+    else {
+        res.send(400, {success: false});
+    }
 });
 
+// Register a new user
 router.post('/', function (req, res) {
     // Require phone, latitude and longitude
     if (req.body && (req.body.phone && req.body.lat && req.body.lng)) {
