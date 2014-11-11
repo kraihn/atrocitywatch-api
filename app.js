219fff25fa9cba99c266840c036b433e1285db38
Jared Dickson <code@jareddickson.com>
2014-11-11 13:21:38 -0800

    Add initial post user route

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/users', users);
 app.use('/api/atrocity', atrocity);
 app.use('/api/status', status);
 
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+    res.send('respond with a resource');
+});
+
+router.post('/', function (req, res) {
+    // Require phone, latitude and longitude
+    if (req.body && (req.body.phone && req.body.lat && req.body.lng)) {
+        // TODO: record data to the database
+        res.send({success: true})
+    }
+    else {
+        res.send(400, {success: false});
+    }
 });
 
 module.exports = router;
