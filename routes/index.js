0f3624d84ec5a8aad641c10f00ab88ebdd760433
Jared Dickson <code@jareddickson.com>
2014-11-11 18:52:31 -0800

    Change index to Atrocity Watch

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  console.log('index');
+  res.render('index', {
+    title: 'Atrocity Watch'
+  });
 });
 
 module.exports = router;
