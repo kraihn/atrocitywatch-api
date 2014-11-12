0a8a40cacf5cc9422d1a4c2c3090a25da61e0a1b
Jared Dickson <code@jareddickson.com>
2014-11-11 19:18:47 -0800

    Add create atrocities

--- routes/atrocity.js
+++ routes/atrocity.js
@@ -1,24 +1,28 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function (req, res) {
-    if (req.body && (req.body.lat && req.body.lng && req.body.type && req.body.description)) {
-        var atrocity = {
-            date: new Date(),
-            latitude: req.body.lat,
-            longitude: req.body.lng,
-            type: req.body.type.split(','),
-            description: req.body.description,
-            severity: req.body.severity,
-            radius: 25
-        };
-        // TODO: record data to the database
-        res.send({success: true})
-    }
-    else {
-        res.send(400, {success: false});
-    }
+  if (req.body && (req.body.lat && req.body.lng)) {
+
+    models.Atrocity.create({
+      latitude: req.body.lat,
+      longitude: req.body.lng,
+      reportedDate: new Date(),
+      severity: 3,
+      type: req.body.type,
+      description: req.body.msg
+    }).success(function (atrocity) {
+      res.send(200, {success: true});
+    }).error(function (error) {
+      res.send(400, {success: false});
+    });
+    
+  }
+  else {
+    res.send(400, {success: false});
+  }
 });
 
 module.exports = router;
